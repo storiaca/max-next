f7e3f9622837513c406d4d4bad0a17c65c364c32
Aleksandar Ristic <storiaca@gmail.com>
2025-10-13 18:48:53 +0200

    start files

---
+++ 001-start/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Image
+        src="/logo.png"
+        alt="A server surrounded by magic sparkles."
+        width="500"
+        height="500"
+      />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Link of course
 
 https://www.udemy.com/course/nextjs-react-the-complete-guide
+
+## Command for new project
+
+```bash
+npx create-next-app@latest .
+```
