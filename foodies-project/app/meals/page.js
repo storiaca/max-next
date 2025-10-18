5e95c1d6e53cc55a9a5d2297ba74841aa6c5bd29
Aleksandar Ristic <storiaca@gmail.com>
2025-10-18 16:19:55 +0200

    Using Suspense & Streamed Responses For Granular Loading State Management

--- foodies-project/app/meals/page.js
+++ foodies-project/app/meals/page.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
-import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import classes from "./page.module.css";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +26,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching loading</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
