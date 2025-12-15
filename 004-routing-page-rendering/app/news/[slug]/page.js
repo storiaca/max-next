7997fde280eb0e2c9dad38d1bf9127679ed1862f
Aleksandar Ristic <storiaca@gmail.com>
2025-12-15 10:12:23 +0100

    App styling and using dummy data

---
+++ 004-routing-page-rendering/app/news/[slug]/page.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default async function NewsSinglePage({ params }) {
+  const { slug } = await params;
+
+  const newsItem = DUMMY_NEWS.find(
+    (newsItem) => newsItem.slug === slug
+  );
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`}
+          alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+
+      <p>{newsItem.content}</p>
+    </article>
+  );
+}
--- 004-routing-page-rendering/app/news/page.js
+++ 004-routing-page-rendering/app/news/page.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsPage() {
   return(
     <div>
       <h1>News Page</h1>
       <ul className="news-list">
-        <li><Link href="/news/1">Some news 1</Link></li>
-        <li><Link href="/news/2">Some news 2</Link></li>
+        {DUMMY_NEWS.map(newsItem => (<li
+        key={newsItem.id}><Link href={`/news/${newsItem.slug}`}><img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <span>{newsItem.title}</span></Link></li>))}
       </ul>
     </div>
   )
