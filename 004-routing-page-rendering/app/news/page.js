421d1fc71743537d27dabf033b73fb9fded35d7d
Aleksandar Ristic <storiaca@gmail.com>
2025-12-16 15:49:40 +0100

    Working with Parallel routes and nested routes

--- 004-routing-page-rendering/app/news/page.js
+++ 004-routing-page-rendering/app/news/page.js
@@ -1,16 +1,12 @@
-import Link from "next/link";
+import NewsList from "@/components/NewsList";
 
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsPage() {
-  return(
+  return (
     <div>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map(newsItem => (<li
-        key={newsItem.id}><Link href={`/news/${newsItem.slug}`}><img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-        <span>{newsItem.title}</span></Link></li>))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </div>
-  )
+  );
 }
