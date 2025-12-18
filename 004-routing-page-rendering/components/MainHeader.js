80baf7fc7e220b4faa847be7dd7c831cf0742d00
Aleksandar Ristic <storiaca@gmail.com>
2025-12-18 16:40:03 +0100

    Add usePathname to header

--- 004-routing-page-rendering/components/MainHeader.js
+++ 004-routing-page-rendering/components/MainHeader.js
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function MainHeader() {
-  return(
+  const paht = usePathname();
+  return (
     <header id="main-header">
       <div id="logo">
         <Link href="/">NextNews</Link>
@@ -10,8 +13,11 @@ export default function MainHeader() {
           <li>
             <Link href="/news">News</Link>
           </li>
+          <li>
+            <Link href="/archive">Archive</Link>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
