ebcc6d4902c1fb73e3f52ab561e3cb5be79691c6
Aleksandar Ristic <storiaca@gmail.com>
2025-12-24 13:26:51 +0100

    Using and understanding Route grous

--- 004-routing-page-rendering/app/(content)/layout.js
+++ 004-routing-page-rendering/app/(content)/layout.js
@@ -1,13 +1,13 @@
 import MainHeader from '@/components/MainHeader';
-import './globals.css';
-
+import '../globals.css'
+ 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
-}
-
-export default function RootLayout({ children }) {
- return (
+};
+ 
+export default function ContentLayout({ children }) {
+  return (
     <html lang="en">
       <body>
         <div id="page">
@@ -16,5 +16,5 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
-  )
+  );
 }
