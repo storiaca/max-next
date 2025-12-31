14c280c604c460d54f0e1740518bfeaf00b3c623
Aleksandar Ristic <storiaca@gmail.com>
2025-12-31 10:13:47 +0100

    Adjusting server acins for useActionState

--- 006-mutate-data/app/new-post/page.js
+++ 006-mutate-data/app/new-post/page.js
@@ -1,12 +1,10 @@
-"use client"
-import { useActionState } from 'react';
 import { redirect } from "next/navigation"
 import { storePost } from "@/lib/posts"
-import FormSubmit from "@/components/form-submit"
+import PostForm from "@/components/post-form"
 
 export default function NewPostPage() {
   
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server"
     const title = formData.get('title')
     const image = formData.get('image')
@@ -22,7 +20,7 @@ export default function NewPostPage() {
       errors.push("Content is required.")
     }
 
-    if(!image) {
+    if(!image || image.size === 0) {
       errors.push("Image is required.")
     }
 
@@ -40,33 +38,8 @@ export default function NewPostPage() {
     redirect('/feed')
   }
 
-  const [state, formAction] = useActionState(createPost, {})
 
   return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+    <PostForm action={createPost}/>  
+  )
 }
