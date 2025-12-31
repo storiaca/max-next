490deb53d19a45774d50ffca427eb995dd701f23
Aleksandar Ristic <storiaca@gmail.com>
2025-12-31 09:31:09 +0100

    Providing User Feedback wth the useFormStatus Hook

--- 006-mutate-data/app/new-post/page.js
+++ 006-mutate-data/app/new-post/page.js
@@ -1,4 +1,6 @@
+import { redirect } from "next/navigation"
 import { storePost } from "@/lib/posts"
+import FormSubmit from "@/components/form-submit"
 
 export default function NewPostPage() {
   async function createPost(formData) {
@@ -7,12 +9,14 @@ export default function NewPostPage() {
     const image = formData.get('image')
     const content = formData.get('content')
 
-    storePost({
+    await storePost({
       imageUrl: '',
       title,
       content,
       userId: 1
     })
+
+    redirect('/feed')
   }
   return (
     <>
@@ -36,8 +40,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
