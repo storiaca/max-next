c39704de3cf411e709997d0f3aca398ff810bb71
Aleksandar Ristic <storiaca@gmail.com>
2026-01-06 09:47:02 +0100

    Making Sene of NextJS Caching types

--- 007-caching/app/messages/page.js
+++ 007-caching/app/messages/page.js
@@ -1,11 +1,13 @@
+import {unstable_noStore} from 'next/cache'
 import Messages from '@/components/messages';
 
+// export const revalidate = 5 // number of seconds
+
+// export const dynamic = 'force-dynamic'
+
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      revalidate: 5
-    }
-  });
+  unstable_noStore()
+  const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
