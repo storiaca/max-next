a0b0bd5fd9eab0c3b882b0b67ccd8cf26afcde15
Aleksandar Ristic <storiaca@gmail.com>
2026-01-06 09:37:28 +0100

    Cache, in header next option, define in seconds

--- 007-caching/app/messages/page.js
+++ 007-caching/app/messages/page.js
@@ -2,7 +2,9 @@ import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
   const response = await fetch('http://localhost:8080/messages', {
-    cache: 'no-store'
+    next: {
+      revalidate: 5
+    }
   });
   const messages = await response.json();
 
