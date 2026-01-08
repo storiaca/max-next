aa076ba620424100f7fc790db739514b506f85bc
Aleksandar Ristic <storiaca@gmail.com>
2026-01-08 13:21:24 +0100

    On-Demand Cache invalidation  with revalidatePath and revalidateTag

--- 007-caching/app/messages/page.js
+++ 007-caching/app/messages/page.js
@@ -3,11 +3,13 @@ import Messages from '@/components/messages';
 
 // export const revalidate = 5 // number of seconds
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
 export default async function MessagesPage() {
   // unstable_noStore()
-  const response = await fetch('http://localhost:8080/messages');
+  const response = await fetch('http://localhost:8080/messages', {
+    next: {tags: ['msg']}
+  });
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
