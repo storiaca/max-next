a8bd0f550ef8577f492f854c6e8fdbcf4856decd
Aleksandar Ristic <storiaca@gmail.com>
2026-01-27 10:26:56 +0100

    Configuring Dynaimc Page Metadata

--- 008-optimizations/app/feed/page.js
+++ 008-optimizations/app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'Browse all our X posts',
+//   description: 'Browse all our posts.'
+// }
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOPosts} posts.`,
+    description: 'Browse all our posts.'
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
