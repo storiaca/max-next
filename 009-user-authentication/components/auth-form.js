3f975036d97802a2b9d2e821f0f1af860cf053e3
Aleksandar Ristic <storiaca@gmail.com>
2026-01-28 14:32:37 +0100

    User signup Extrtracting and validating user input

--- 009-user-authentication/components/auth-form.js
+++ 009-user-authentication/components/auth-form.js
@@ -1,8 +1,14 @@
+'use client'
 import Link from 'next/link';
+import { useActionState } from 'react'
+import { signup } from '@/actions/auth-actions';
 
 export default function AuthForm() {
+  const [formState, formAction] = useActionState(signup, {
+    errors: {}
+  })
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,6 +20,9 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && (<ul id="form-errors">
+        {Object.keys(formState.errors).map(error => <li key={error}>{formState.errors[error]}</li>)}
+      </ul>)}
       <p>
         <button type="submit">
           Create Account
